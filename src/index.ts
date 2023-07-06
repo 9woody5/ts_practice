b8948f4be7d4ac7625b24d73532c768d4a5d6a20
9woody5 <woojin958@gmail.com>
2023-07-07 02:18:59 +0900

    type check and JSDoc

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+import { init, exit } from "./myPackage";
+//d.ts 파일에 declare되어야 에러없이 동작함
---
+++ src/myPackage.js
@@ -0,0 +1,23 @@
+//js파일에 대한 보호 장치를 추가하는 것. ts가 js를 확인함
+//JSDoc - 코멘트로 이루어진 문법
+
+// @ts-check
+/**
+ * Initializes the project
+ * @param {object} config 
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns boolean
+ */
+export function init(config) {
+    return true;
+}
+
+/**
+ * Exits the program
+ * @param {number} code 
+ * @returns number
+ */
+export function exit(code) {
+    return code + 1;
+}
